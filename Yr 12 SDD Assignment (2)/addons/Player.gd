755bcc29bd60a1ab452d18619e670809f20f07ee
DrTeletubby1 <91893399+DrTeletubby1@users.noreply.github.com>
2024-05-21 02:40:25 +1000

    Add files via upload

--- Yr 12 SDD Assignment (2)/addons/Player.gd
+++ Yr 12 SDD Assignment (2)/addons/Player.gd
@@ -1,40 +1,25 @@
 extends CharacterBody3D
-@onready var Animated_sprite_2d = $CanvasLayer/GunBase/AnimatedSprite2D
-@onready var ray_cast_3d = $RayCast3D
-@onready var shoot_sound = $ShootSound
+
 
 const SPEED = 5.0
-const MOUSE_SENS = 0.5
+const JUMP_VELOCITY = 4.5
 
-var can_shoot = true
-var dead = false
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	Animated_sprite_2d.animation_finished.connect(shoot_anim_done)
-	$CanvasLayer/Deathscreen/panel/Button.button_up.connect(_restart)
-
-func _input(event):
-	if dead:
-		return
-	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x * MOUSE_SENS
-
-func _process(delta):
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("restart"):
-		_restart()
-
-	if dead:
-		return
-	if Input.is_action_just_pressed("Shoot"):
-		_shoot()
 
 func _physics_process(delta):
-	if dead:
-		return
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -44,23 +29,3 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-func _restart():
-	get_tree().reload_current_scene()
-
-func _shoot():
-	if !can_shoot:
-		return
-	can_shoot = false
-	Animated_sprite_2d.play("shoot")
-	shoot_sound.play()
-	if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider().has_method("kill"):
-		ray_cast_3d.get_collider().kill()
-
-func shoot_anim_done():
-	can_shoot = true
-
-func kill():
-	dead = true
-	$CanvasLayer/Deathscreen.show()
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
--- Yr 12 SDD Assignment (2)/player.gd
+++ Yr 12 SDD Assignment (2)/player.gd
@@ -1,46 +1,69 @@
-extends KinematicBody
- 
-const MOVE_SPEED = 4
+extends CharacterBody3D
+
+@onready var animated_sprite_2d = $CanvasLayer/GunBase/AnimatedSprite2D
+@onready var ray_cast_3d = $Raycast3D
+@onready var shoot_sound = $ShootSound
+
+
+const SPEED = 5.0
 const MOUSE_SENS = 0.5
- 
-onready var anim_player = $AnimationPlayer
-onready var raycast = $RayCast
- 
+
+var can_shoot = true
+var dead = false
+
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	await(get_tree(), "idle_frame")
-	get_tree().call_group("zombies", "set_player", self)
- 
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	animated_sprite_2d.animation_finished.connect(shoot_anim_done)
+	$CanvasLayer/DeathScreen/Panel/Button.button_up.connect(restart)
+
 func _input(event):
+	if dead:
+		return
 	if event is InputEventMouseMotion:
-		rotation_degrees.y -= MOUSE_SENS * event.relative.x
- 
-func _process(delta):
-	if Input.is_action_pressed("exit"):
+		rotation_degrees.y -= event.relative.x * MOUSE_SENS
+
+func _process(_delta):
+	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
-	if Input.is_action_pressed("restart"):
-		kill()
- 
-func _physics_process(delta):
-	var move_vec = Vector3()
-	if Input.is_action_pressed("move_forwards"):
-		move_vec.z -= 1
-	if Input.is_action_pressed("move_backwards"):
-		move_vec.z += 1
-	if Input.is_action_pressed("move_left"):
-		move_vec.x -= 1
-	if Input.is_action_pressed("move_right"):
-		move_vec.x += 1
-	move_vec = move_vec.normalized()
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-	move_and_collide(move_vec * MOVE_SPEED * delta)
- 
-	if Input.is_action_pressed("shoot") and !anim_player.is_playing():
-		anim_player.play("shoot")
-		var coll = raycast.get_collider()
-		if raycast.is_colliding() and coll.has_method("kill"):
-			coll.kill()
- 
-func kill():
+	if Input.is_action_just_pressed("restart"):
+		restart()
+
+	if dead:
+		return
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func _physics_process(_delta):
+	if dead:
+		return
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func restart():
 	get_tree().reload_current_scene()
 
+func shoot():
+	if !can_shoot:
+		return
+	can_shoot = false
+	animated_sprite_2d.play("shoot")
+	shoot_sound.play()
+	if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider().has.method("kill"):
+		ray_cast_3d.get_collider().kill()
+
+func shoot_anim_done():
+	can_shoot = true
+
+func kill():
+	dead = true
+	$canvasLayer/DeathScreen.show()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
